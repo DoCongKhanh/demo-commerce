9ddd0e926bd34a4b04e1ef1d05f3c51d99c9deff
DoCongKhanh <khanhdo2266@gmail.com>
2022-11-21 10:01:40 +0700

    make it better

--- myshop/settings.py
+++ myshop/settings.py
@@ -143,7 +143,7 @@ STATIC_URL = '/static/'
 # STATIC_ROOT = '/static/'
 
 STATICFILES_DIR = ( 
-    os.path.join(BASE_DIR, 'static')
+    os.path.join(BASE_DIR, 'static'),
 )
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 django_heroku.settings(locals())
