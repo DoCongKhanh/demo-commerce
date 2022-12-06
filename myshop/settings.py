ed3de8e593808a36d3829dd3f83d1d1711916d3d
DoCongKhanh <khanhdo2266@gmail.com>
2022-12-06 16:08:10 +0700

    done code

--- myshop/settings.py
+++ myshop/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-qk$=er3al7i6m)_x_kg=&e+vo3et4#4y%qy3!ku6-0!+m)p#-6
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
+ALLOWED_HOSTS = []
 
 # chuyen page khi login va logout
 LOGIN_URL = 'login'
@@ -139,7 +139,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 # STATIC_ROOT = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_DIR = (os.path.join(BASE_DIR, 'static'),)
 
